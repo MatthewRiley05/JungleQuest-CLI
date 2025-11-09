2aaccd1064d44b4ac27fdcc6503378be6af8be9e
MatthewRiley05 <matthewrileyraymundo@gmail.com>
2025-11-10 01:18:10 +0800

    chore: remove Python cache files and add .gitignore to prevent future cache file commits

--- controller/controller.py
+++ controller/controller.py
@@ -6,6 +6,7 @@ from view.view import View
 
 import re
 
+
 class Controller:
     def __init__(self):
         self.view = View()
@@ -24,60 +25,78 @@ class Controller:
             self.view.display_turn(self.game.players[self.game.current_turn].name)
 
             move = self.view.get_user_input()
-            if move.lower() == 'quit':
+            if move.lower() == "quit":
                 print("Terminating game session...")
                 break
 
             # attempt to take a turn
-            if not self.take_turn(move):  
+            if not self.take_turn(move):
                 continue  # await new input from the user
-            
 
             self.game.switch_turn()
-    
-    def parse_move_input(self, input : str):
-        pattern = r'^[A-Ga-g][1-9] to [A-Ga-g][1-9]$' # valid characters include A-G, a-g, 1-9
-        
+
+    def parse_move_input(self, input: str):
+        pattern = r"^[A-Ga-g][1-9] to [A-Ga-g][1-9]$"  # valid characters include A-G, a-g, 1-9
+
         if not re.match(pattern, input):
-            print("Invalid input format. Please enter a valid move (e.g: A1 to A2, B4 to C4)")
-            return None, None # means input is invalid.
-        
+            print(
+                "Invalid input format. Please enter a valid move (e.g: A1 to A2, B4 to C4)"
+            )
+            return None, None  # means input is invalid.
+
         from_part, to_part = input.split(" to ")
 
-        if not self.convert_to_coordinates(from_part) or not self.convert_to_coordinates(to_part):
-            print("Input is out of bounds. Please enter a valid move (e.g: A1 to A2, B4 to C4)")
+        if not self.convert_to_coordinates(
+            from_part
+        ) or not self.convert_to_coordinates(to_part):
+            print(
+                "Input is out of bounds. Please enter a valid move (e.g: A1 to A2, B4 to C4)"
+            )
             return None, None
         from_position = self.convert_to_coordinates(from_part)
         to_position = self.convert_to_coordinates(to_part)
 
         return from_position, to_position
-    
-    def is_valid_move(self, from_position : tuple[int, int], to_position : tuple[int, int]) -> bool: 
+
+    def is_valid_move(
+        self, from_position: tuple[int, int], to_position: tuple[int, int]
+    ) -> bool:
         row_difference = abs(from_position[0] - to_position[0])
         col_difference = abs(from_position[1] - to_position[1])
-        
+
         # Check if the movement is only one tile upwards, downwards, left or right.
-        if (row_difference + col_difference != 1):  
+        if row_difference + col_difference != 1:
             return False
-        
-        piece : Piece = self.game.board.get_piece(from_position)
-        # Check if the piece is owned by the current player. 
+
+        piece: Piece = self.game.board.get_piece(from_position)
+        # Check if the piece is owned by the current player.
         # note: current_turn = 0 if player 1's turn, 1 if player 2's turn.
-        if self.game.current_turn != piece.owner and (self.game.current_turn != piece.owner):
-            return False 
-        
-        # Check if the move leads to the player's own den
-        if self.game.current_turn == 0 and to_position == self.game.board.PLAYER_1_DEN_POSITION:
+        if self.game.current_turn != piece.owner and (
+            self.game.current_turn != piece.owner
+        ):
             return False
-        if self.game.current_turn == 1 and to_position == self.game.board.PLAYER_2_DEN_POSITION:
+
+        # Check if the move leads to the player's own den
+        if (
+            self.game.current_turn == 0
+            and to_position == self.game.board.PLAYER_1_DEN_POSITION
+        ):
+            return False
+        if (
+            self.game.current_turn == 1
+            and to_position == self.game.board.PLAYER_2_DEN_POSITION
+        ):
             return False
 
         # Check if a non-rat piece tried to move into a water tile.
-        if piece.name != 'Rat' and self.game.board.get_tile(to_position).tile_type == Tile.WATER:
-            return False 
-        
+        if (
+            piece.name != "Rat"
+            and self.game.board.get_tile(to_position).tile_type == Tile.WATER
+        ):
+            return False
+
         return True
-    
+
     def convert_to_coordinates(self, position):
         # Convert a position string like "A1" to a tuple (row, column)
         # column = ord(position[0].upper()) - ord('A')  # Convert 'A'-'G' to 0-6
@@ -86,43 +105,50 @@ class Controller:
         column = -1
         row = -1
         match position[0].upper():
-            case 'A': column = 0
-            case 'B': column = 1
-            case 'C': column = 2
-            case 'D': column = 3
-            case 'E': column = 4
-            case 'F': column = 5
-            case 'G': column = 6
-        row = int(position[1]) - 1 
+            case "A":
+                column = 0
+            case "B":
+                column = 1
+            case "C":
+                column = 2
+            case "D":
+                column = 3
+            case "E":
+                column = 4
+            case "F":
+                column = 5
+            case "G":
+                column = 6
+        row = int(position[1]) - 1
         return (column, row)
-    
 
-    
     def take_turn(self, move):
         # Validate the move string format
         from_position, to_position = self.parse_move_input(move)
 
-
         if not from_position or not to_position:
             return  # for invalid inputs
 
-        current_player = self.game.current_turn # 0 for player 1, 1 for player 2
-        piece_to_move : Piece = self.game.board.get_piece(from_position)
+        current_player = self.game.current_turn  # 0 for player 1, 1 for player 2
+        piece_to_move: Piece = self.game.board.get_piece(from_position)
 
         if piece_to_move is None:
             print("Invalid move. A tile with no piece was selected. Please try again.")
             return
-        
 
-        if self.is_valid_move(from_position, to_position) == False:
-            print("Invalid move. You may only move your own piece by one tile horizontally/vertically, and never into your its own den or water (except rats)")
-            return 
+        if not self.is_valid_move(from_position, to_position):
+            print(
+                "Invalid move. You may only move your own piece by one tile horizontally/vertically, and never into your its own den or water (except rats)"
+            )
+            return
 
         # Check if the target tile is occupied
-        target_tile : Tile = self.game.board.get_tile(to_position)
-        if target_tile != None:
-            if target_tile.get_piece() != None and ():
-                print("Invalid move: You cannot move to a tile occupied by your own piece.")
+        target_tile: Tile = self.game.board.get_tile(to_position)
+        if target_tile is not None:
+            if target_tile.get_piece() is not None and ():
+                print(
+                    "Invalid move: You cannot move to a tile occupied by your own piece."
+                )
                 return
             # WIP
             # else:
@@ -135,12 +161,11 @@ class Controller:
 
         # End turn
         self.end_turn()
-    
+
     # WIP
     def capture_piece(self, position):
         # Logic to handle capturing an opponent's piece
         print(f"Captured piece at {position}")
-
 
     def end_turn(self):
         # Switch to the next player
